819957c528088a18ee6503a19d3a398226324a99
Danny Allen <dannyallen@dannys-mac.local>
2019-03-19 15:37:14 -0700

    Initial Commit

---
+++ Voice-to-Text/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Voice-to-Text
+//
+//  Created by Danny Allen on 3/19/19.
+//  Copyright © 2019 Danny Allen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Voice-to-TextTests/Voice_to_TextTests.swift
@@ -0,0 +1,34 @@
+//
+//  Voice_to_TextTests.swift
+//  Voice-to-TextTests
+//
+//  Created by Danny Allen on 3/19/19.
+//  Copyright © 2019 Danny Allen. All rights reserved.
+//
+
+import XCTest
+@testable import Voice_to_Text
+
+class Voice_to_TextTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
