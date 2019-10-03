49893bb29ba88be338894ff76eff1ade50bed68c
Evan Yang <evan.yang@uky.edu>
2019-10-03 15:45:54 -0400

    updated login storyboard

---
+++ src/ios/dsi/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  dsi
+//
+//  Created by Evan Yang on 10/3/19.
+//  Copyright © 2019 Najwa. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+        self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
