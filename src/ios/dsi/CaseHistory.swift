0ed1e2283e4ee62c5ab00376a76193b09dc41104
eurashin <eura.shin@gmail.com>
2019-11-17 19:09:42 -0500

    Adding http requests

--- src/ios/dsi/CaseHistory.swift
+++ src/ios/dsi/CaseHistory.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class Case_History: UIViewController, UITextFieldDelegate {
     
--- src/ios/dsi/CaseNumberController.swift
+++ src/ios/dsi/CaseNumberController.swift
@@ -7,21 +7,27 @@
 //
 
 import UIKit
+import SearchTextField
 import Alamofire
 
+// Create Global Struct
+var allVar = MDSIDocProperties()
+
 class CaseNumberController: UIViewController {
-    
-    @IBOutlet weak var countyField: UITextField!
+    @IBOutlet weak var countyField: SearchTextField!
     @IBOutlet weak var caseNumberField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        autocompleteOptions()
     }
     
 
     @IBAction func createCasePushed(_ sender: Any) {
+        // Save global case number variable value
+        allVar.CaseNum = caseNumberField
+        allVar.County = countyField
+        
         // Make sure the case number has been entered
         guard let casenum = caseNumberField.text, !casenum.isEmpty,
         let county = countyField.text, !county.isEmpty
@@ -30,6 +36,7 @@ class CaseNumberController: UIViewController {
                 return
             }
         
+
         let url = "https://statsqltest.as.uky.edu/start_death_scene_investigation.php"
         let params: Parameters=[ // ADD INVESTIGATOR LATER IN DB
                 "casenum":casenum,
@@ -47,8 +54,6 @@ class CaseNumberController: UIViewController {
                 if(jsonData.contains("success")){
                     self.caseNumberField.text = ""
                     self.goToDeathSceneInvestigation()
-                    
-                    
                 }
                 else {
                     // Display an alert if an error and database insert didn't work
@@ -74,5 +79,129 @@ class CaseNumberController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func autocompleteOptions() {
+        countyField.filterStrings([
+        "Adair",
+        "Allen",
+        "Anderson",
+        "Ballard",
+        "Barren",
+        "Bath",
+        "Bell",
+        "Boone",
+        "Bourbon",
+        "Boyd",
+        "Boyle",
+        "Bracken",
+        "Breathitt",
+        "Breckinridge",
+        "Bullitt",
+        "Butler",
+        "Caldwell",
+        "Calloway",
+        "Campbell",
+        "Carlisle",
+        "Carroll",
+        "Carter",
+        "Casey",
+        "Christian",
+        "Clark",
+        "Clay",
+        "Clinton",
+        "Crittenden",
+        "Cumberland",
+        "Daviess",
+        "Edmonson",
+        "Elliott",
+        "Estill",
+        "Fayette",
+        "Fleming",
+        "Floyd",
+        "Franklin",
+        "Fulton",
+        "Gallatin",
+        "Garrard",
+        "Grant",
+        "Graves",
+        "Grayson",
+        "Green",
+        "Greenup",
+        "Hancock",
+        "Hardin",
+        "Harlan",
+        "Harrison",
+        "Hart",
+        "Henderson",
+        "Henry",
+        "Hickman",
+        "Hopkins",
+        "Jackson",
+        "Jefferson",
+        "Jessamine",
+        "Johnson",
+        "Kenton",
+        "Knott",
+        "Knox",
+        "Larue",
+        "Laurel",
+        "Lawrence",
+        "Lee",
+        "Leslie",
+        "Letcher",
+        "Lewis",
+        "Lincoln",
+        "Livingston",
+        "Logan",
+        "Lyon",
+        "Madison",
+        "Magoffin",
+        "Marion",
+        "Marshall",
+        "Martin",
+        "Mason",
+        "McCracken",
+        "McCreary",
+        "McLean",
+        "Meade",
+        "Menifee",
+        "Mercer",
+        "Metcalfe",
+        "Monroe",
+        "Montgomery",
+        "Morgan",
+        "Muhlenberg",
+        "Nelson",
+        "Nicholas",
+        "Ohio",
+        "Oldham",
+        "Owen",
+        "Owsley",
+        "Pendleton",
+        "Perry",
+        "Pike",
+        "Powell",
+        "Pulaski",
+        "Robertson",
+        "Rockcastle",
+        "Rowan",
+        "Russell",
+        "Scott",
+        "Shelby",
+        "Simpson",
+        "Spencer",
+        "Taylor",
+        "Todd",
+        "Trigg",
+        "Trimble",
+        "Union",
+        "Warren",
+        "Washington",
+        "Wayne",
+        "Webster",
+        "Whitley",
+        "Wolfe",
+        "Woodford"])
+    }
 
 }
