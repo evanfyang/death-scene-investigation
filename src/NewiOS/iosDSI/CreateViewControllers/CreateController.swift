6b23cff45d59370ddb038d66b18c3368feb861e1
abmajda <abmajda@gmail.com>
2020-10-20 19:58:10 -0400

    Started View Controllers

--- src/NewiOS/iosDSI/CreateViewControllers/CreateController.swift
+++ src/NewiOS/iosDSI/CreateViewControllers/CreateController.swift
@@ -15,11 +15,31 @@ class CreateController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBOutlet weak var CaseNumber: UITextField!
-    @IBOutlet weak var County: UITextField!
+    @IBOutlet weak var caseNum: UITextField!
+    @IBOutlet weak var county: UITextField!
     
+    // Button!
     @IBAction func Create(_ sender: Any) {
-        Storage.Create.CaseNumber = CaseNumber.text!
-        Storage.Create.County = County.text!
+        
+        var correct = true
+        
+        if caseNum.text != "" {}
+        else {
+            correct = false
+        }
+        
+        if county.text! != "Jefferson" || county.text != "" {
+            correct = false
+        }
+        
+        if correct {
+            Storage.Create.CaseNumber = caseNum.text!
+            Storage.Create.County = county.text!
+            
+            // Transition here
+        }
+        else {
+            print("wrong")
+        }
     }
 }
